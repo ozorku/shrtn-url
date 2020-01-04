eab34982d02011c6ca896e120b0ecc998932dd30
adavize <adavize@thepoint.africa>
2020-01-04 07:03:57 +0100

    remove aos

--- src/main.js
+++ src/main.js
@@ -1,16 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Clipboard from 'v-clipboard'
-import AOS from "aos";
-import "aos/dist/aos.css";
 
 Vue.config.productionTip = false
 Vue.use(Clipboard)
 
 
 new Vue({
-  created() {
-    AOS.init({ disable: "phone" });
-  },
   render: h => h(App),
 }).$mount('#app')
